e92c802f92a5dfe3a853d58149bf32aae1d3c5a6
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-01-09 15:21:13 +0300

    fix: fix play pause

--- src/components/shared/PlayPause.tsx
+++ src/components/shared/PlayPause.tsx
@@ -29,7 +29,14 @@ const PlayPause: React.FC<PlayPauseProps> = ({ activeSong, item, i, data }) => {
   return (
     <TrackButton type='button' onClick={handlePlayPause}>
       {activeSong === item && active ? (
-        <svg xmlns='http://www.w3.org/2000/svg' width='18' height='22' viewBox='0 0 18 26' fill='none'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='18'
+          height='22'
+          viewBox='0 0 18 26'
+          fill='none'
+          style={{ verticalAlign: 'middle' }}
+        >
           <rect width='5' height='26' rx='2.5' fill='url(#paint0_linear_9_59)' />
           <rect x='13' width='5' height='26' rx='2.5' fill='url(#paint1_linear_9_59)' />
           <defs>
@@ -58,7 +65,14 @@ const PlayPause: React.FC<PlayPauseProps> = ({ activeSong, item, i, data }) => {
           </defs>
         </svg>
       ) : (
-        <svg xmlns='http://www.w3.org/2000/svg' width='19' height='22' viewBox='0 0 19 22' fill='none'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='19'
+          height='22'
+          viewBox='0 0 19 22'
+          fill='none'
+          style={{ verticalAlign: 'middle' }}
+        >
           <path
             d='M18.5 10.134C19.1667 10.5189 19.1667 11.4811 18.5 11.866L2 21.3923C1.33333 21.7772 0.5 21.2961 0.5 20.5263L0.5 1.47372C0.5 0.703919 1.33333 0.222795 2 0.607695L18.5 10.134Z'
             fill='#B8B8B8'
