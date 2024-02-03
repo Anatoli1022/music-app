afedc4f326ea40e1dcc90820075d94525e75da61
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-02-04 00:49:15 +0300

    delete: delete about page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import GlobalStyle from './theme/globalStyles'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import MainPage from './pages/MainPage'
-import AboutPage from './pages/AboutPage'
+
 const App: React.FC = () => {
   return (
     <>
@@ -12,7 +12,6 @@ const App: React.FC = () => {
         <Routes>
           <Route path='/' element={<MainLayout />}>
             <Route index element={<MainPage />} />
-            <Route path='about' element={<AboutPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
