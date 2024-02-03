a291bb6e5352f37a9ce1e8e218625f1da6fedb7b
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-02-04 00:40:59 +0300

    fix: fix provider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
-import {store} from './redux/store'
-
-ReactDOM.render(
+import { store } from './redux/store'
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </Provider>,
-  document.getElementById('root'),
 )
-
-
